28bbbaa4122af64b3fb2c7c5010b13be7f36a2fa
Mohammed Amin <mhmmdmnsdk@gmail.com>
2026-06-29 10:21:31 +0300

    k

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
-import "package:firebase_auth/firebase_auth.dart";
 import "package:flutter/material.dart";
-
 import 'package:firebase_core/firebase_core.dart';
+import "package:firebase_auth/firebase_auth.dart";
 import 'firebase_options.dart';
 
 import "package:chatting_app/screens/auth.dart";
-import "package:chatting_app/screens/chat_screen.dart";
+import "package:chatting_app/screens/splash.dart";
+import "package:chatting_app/screens/chat.dart";
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -29,6 +29,9 @@ class App extends StatelessWidget {
       home: StreamBuilder(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          }
           if (snapshot.hasData) {
             return const ChatScreen();
           }
